d2e8309344d8a3fb5cc40083215a8cce4fd3ffac
abhinavgupta371 <abhinavgupta371@gmail.com>
2019-02-28 20:49:24 +0530

    Branding: Welcome to Evolution X!
    Change-Id: I284d339161d2d85233e92f01c32a3f322f86845d

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -818,7 +818,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Comment("Stage 3/3")
 
   # Dump fingerprints
-  script.Print("Target: {}".format(target_info.fingerprint))
+  #script.Print("Target: {}".format(target_info.fingerprint))
 
   is_system_as_root = target_info.get("system_root_image") == "true"
   if is_system_as_root and not common.system_as_system:
@@ -830,14 +830,19 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   android_version = target_info.GetBuildProp("ro.build.version.release")
   build_id = target_info.GetBuildProp("ro.build.id")
-  build_date = target_info.GetBuildProp("org.pixelexperience.build_date")
+  build_date = target_info.GetBuildProp("org.evolutionx.build_date")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
-  device = target_info.GetBuildProp("org.pixelexperience.device")
+  device = target_info.GetBuildProp("org.evolutionx.device")
 
-  script.Print("----------------------------------------------");
-  script.Print("              Pixel Experience");
-  script.Print("               by jhenrique09");
-  script.Print("----------------------------------------------");
+  script.Print("----------------------------------------------------");
+  script.Print("    ______            __      __  _            _  __");
+  script.Print("   / ____/   ______  / /_  __/ /_(_)___  ____ | |/ /");
+  script.Print("  / __/ | | / / __ \/ / / / / __/ / __ \/ __ \|   / ");
+  script.Print(" / /___ | |/ / /_/ / / /_/ / /_/ / /_/ / / / /   |  ");
+  script.Print("/_____/ |___/\____/_/\__,_/\__/_/\____/_/ /_/_/|_|  ");
+  script.Print("                                                    ");
+  script.Print("                  By Stallix & Agrd1129             ");
+  script.Print("----------------------------------------------------");
   script.Print(" Android version: %s"%(android_version));
   script.Print(" Build id: %s"%(build_id));
   script.Print(" Build date: %s"%(build_date));
@@ -869,6 +874,9 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     system_progress -= 0.1
   if HasVendorPartition(input_zip):
     system_progress -= 0.1
+
+  model = target_info.GetBuildProp("ro.product.model")
+  build = target_info.GetBuildProp("ro.build.date")
 
   script.ShowProgress(system_progress, 0)
 
